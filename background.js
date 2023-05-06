647cbd2c7eca1e57be19cd791a5fa3dbb81383e9
Amit Lacher <a.lacher@tricentis.com>
2023-05-06 23:00:16 +0300

    feat: updated background
    Signed-off-by: Amit Lacher <a.lacher@tricentis.com>

--- background.js
+++ background.js
@@ -1,106 +1,52 @@
+import * as consts from "/consts.js";
+import * as helpers from "/helpers.js";
+import * as locators from "/locators.js";
+
 function sdarotAutoPlay() {
-    console.log('Sdarot Auto Play is Enabled');
-    let isSpinnerHiddenChange = false;
-    let isTryingToLog = false;
-    const SDAROT_USERNAME_KEY = 'sdarotUsername';
-    const SDAROT_PASSWORD_KEY = 'sdarotPassword';
-    const NOT_LOGGED_ERROR_TEXT = 'שגיאה 2!על מנת לצפות בפרק עליך להתחבר למערכת';
-    const SERVER_LOAD_ERROR_TEXT = 'שגיאה 2!כל שרתי הצפייה שלנו עמוסים ולא יכולים לטפל בפניות נוספות, נא לנסות שנית מאוחר יותר.לצפייה בעומסי השרתים לחצו כאן.משתמשים שתרמו לאתר יכולים לצפות בפרקים גם בשעות העומס!';
-    const sleep = (miliseconds) => new Promise((res) => {
-        setTimeout(() => {
-            res();
-        }, miliseconds);
-    })
+    const currentVersion = "1.1.0";
+    const probingTimeout = 1000;
+    let probCount = 0;
+    let logging = false;
 
-    const getSdarotCredentials = (key) => new Promise((res) => {
-        chrome.storage.sync.get([key], (result) => {
-            res(result[key]);
-        });
-    })
-
-    const login = (username, password) => {
-        if (!isTryingToLog) {
-            isTryingToLog = true;
-            fetch("https://sdarot.buzz/login", {
-                "headers": {
-                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                    "accept-language": "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
-                    "cache-control": "no-cache",
-                    "content-type": "application/x-www-form-urlencoded",
-                    "pragma": "no-cache",
-                    "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-ch-ua-platform": "\"Windows\"",
-                    "sec-fetch-dest": "document",
-                    "sec-fetch-mode": "navigate",
-                    "sec-fetch-site": "same-origin",
-                    "sec-fetch-user": "?1",
-                    "upgrade-insecure-requests": "1"
-                },
-                "referrer": "https://sdarot.buzz/",
-                "referrerPolicy": "strict-origin",
-                "body": `location=%2Fwatch%2F8327-%25D7%2591%25D7%2599%25D7%25AA-%25D7%2594%25D7%2593%25D7%25A8%25D7%25A7%25D7%2595%25D7%259F-house-of-the-dragon%2Fseason%2F1%2Fepisode%2F9&username=${username}&password=${password}&submit_login=`,
-                "method": "POST",
-                "mode": "cors",
-                "credentials": "include"
-            }).then(() => {
-                window.location.reload();
-            }).catch((error) => {
-                console.log('Error while trying to log in');
-            });
-        }
-    }
+    console.log(`Sdarot Auto Play version ${currentVersion} is Enabled`);
 
     const interval = setInterval(async () => {
-        const errElement = document.querySelectorAll(".err");
-        if (errElement?.length > 0 && errElement[0].textContent === NOT_LOGGED_ERROR_TEXT) {
-            console.log('You need to log in first');
-            const sdarotUsername = await getSdarotCredentials(SDAROT_USERNAME_KEY);
-            const sdarotPassword = await getSdarotCredentials(SDAROT_PASSWORD_KEY);
-            if (sdarotPassword && sdarotUsername) {
-                login(sdarotUsername, sdarotPassword);
+        console.log(`Searching for errors. Probing count is ${probCount}`)
+        if (locators.IsNotLoggedErrorVisible()) {
+            console.log('Found user logging error, logging in...');
+            const [sdarotUsername, sdarotPassword] = helpers.getStoredCredentials([consts.SDAROT_USERNAME_KEY, consts.SDAROT_PASSWORD_KEY]);
+
+            if (!logging && sdarotPassword && sdarotUsername) {
+                helpers.login(sdarotUsername, sdarotPassword);
+                logging = true;
             }
             clearInterval(interval);
-        } else if (errElement?.length > 0 && errElement[0].textContent === SERVER_LOAD_ERROR_TEXT) {
-            console.log('Found loading error');
+        } else if (locators.IsServerErrorVisible()) {
+            console.log('Found server loading error, reloading...');
             window.location.reload();
         } else {
-            const spinner = document.querySelector("#spinner");
-            if (spinner && window.getComputedStyle(spinner)?.display !== 'none') {
-                isSpinnerHiddenChange = true;
-            } else if (isSpinnerHiddenChange) {
-                const playEpisode = document.querySelector("#proceed");
-                playEpisode.click();
-                await sleep(1500);
-                document.querySelector('.vjs-big-play-button > .vjs-icon-placeholder').click();
-                await sleep(1500);
-                document.querySelector('.vjs-fullscreen-control').click();
+            if (locators.IsSpinnerVisible()) {
+                locators.GetEpisodeToPlay().click();
+                await helpers.sleep();
+
+                locators.GetPlayBtn().click();
+                await helpers.sleep();
+
+                locators.GetExpandViewBtn().click();
+
                 clearInterval(interval);
             }
         }
-    }, 1000)
+    }, probingTimeout)
 }
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url?.includes('episode')) {
+function initSdarotAutoplay(tabId, info) {
+    if (info.url?.includes('episode')) {
         chrome.scripting.executeScript({
-            target: { tabId: tabId, allFrames: true },
+            target: {tabId: tabId, allFrames: true},
             func: sdarotAutoPlay,
         });
     }
-})
+}
 
-chrome.webNavigation.onCommitted.addListener((details) => {
-    if (details.url?.includes("episode")) {
-        chrome.scripting.executeScript({
-            target: { tabId: details.tabId, allFrames: true },
-            func: sdarotAutoPlay,
-        });
-    }
-}, {
-    url: [
-        {
-            hostContains: 'sdarot'
-        },
-    ]
-})
+export {initSdarotAutoplay}
